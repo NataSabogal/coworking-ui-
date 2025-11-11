a5422ebbc20b7ae19ae156c93a8db29eba5e9ae8
natasabogal <natalia.sabogal.1766@eam.edu.co>
2025-11-10 21:54:28 -0500

    Prueba repo UI

---
+++ .angular/cache/20.3.9/coworkin-project-ui/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-CFOSJRO3.js";
+import "./chunk-2B4U7YA7.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
